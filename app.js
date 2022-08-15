88b033894f4320b26cb234a9f5bc2b86c4c5b4f6
Irene Santos <ireneptc@hotmail.com>
2022-08-14 21:54:50 -0300

    Update desafio

---
+++ app.js
@@ -0,0 +1,34 @@
+let botaoAtualizar = document.getElementById('atualizar-saldo');
+let botaoLimpar = document.getElementById('limpar-saldo');
+let soma = document.getElementById('soma');
+let campoSaldo = document.getElementById('campo-saldo');
+
+campoSaldo.innerHTML = 0;
+saldoTotal = 0;
+
+function somarAoSaldo(soma) {
+    saldoTotal = soma + saldoTotal;
+    console.log(soma)
+    console.log(typeof(soma))
+    console.log(saldoTotal);
+    campoSaldo.innerHTML = saldoTotal;
+}
+
+function limparSaldo() {
+    campoSaldo.innerHTML = '';
+}
+
+botaoAtualizar.addEventListener('click', function () {
+    somarAoSaldo(Number(soma.value));
+});
+
+botaoLimpar.addEventListener('click', function () {
+    limparSaldo();
+});
+
+/**
+    <h4>Valor a ser adicionado: <input id="soma"> </h4>
+    <button id="atualizar-saldo">Atualizar saldo</button>
+    <button id="limpar-saldo">Limpar seu saldo</button>
+    <h1>"Seu saldo é: " <span id="campo-saldo"></span></h1>
+ */
